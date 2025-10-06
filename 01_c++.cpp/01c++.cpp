81a145cbe6d74938f410dcca14eda6d491ee1426
Divyansh Savita <savitadivyansh323@gmail.com>
2025-10-06 16:02:26 +0530

    file added to dsa

---
+++ 01_c++.cpp/01c++.cpp
@@ -0,0 +1,83 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    // int x=5;
+    // cout<<x++;
+    // cout<<++x;
+    // int a=4;
+    // int b=5;
+    // cout<<a%b;//if a is less the a%b==a
+
+
+    // Simple interest
+    // float principle=4000,rate=2,time=5;
+    // float si=(principle*rate*time)/100;
+    // cout<<si;
+
+    //input
+    // float principal;
+    // cout<<"enter principal";
+    // cin>>principal;
+    // float rate;
+    // cout<<"enter rate";
+    // cin>>rate;
+    // float time;
+    // cout<<"enter time";
+    // cin>>time;
+    // float si=(principal*rate*time)/100;
+    // cout<<"simple interest : "<<si;
+
+    //char datatype ,type casting   'A'=>65,'a'=>97,'0'=>48,'9'=>57   
+    // char ch='a';
+    // cout<<(int)ch;
+
+    // bool x=true;
+    // cout<<x+9;
+    // int y=(int) x;
+
+    // char input;
+    // cout<<"give the input ";
+    // cin>>input;
+    // int result=(int)(input-64);
+    // cout<<result;
+
+    // float inputt;
+    // cout<<"give input for fractional part of real numbber";
+    // cin>>inputt;
+    // float result;
+    // if(inputt>0){
+    //     result=inputt-(int)inputt;
+    // }else{
+    //     if(typeid(inputt) == typeid(int)){
+    //         result=inputt-inputt;
+    //         cout<<result;
+    //         return result;
+    //     }
+    //     result=inputt-((int)inputt-1);
+    // }
+    // cout<<result;
+
+    // float x;
+    // cin>>x;
+    // int y=(int)x;
+    // if(y<0)y=y-1;
+    // float z=(float)y;
+    // x=x-z;
+    // cout<<x;
+
+
+    // string a="physics";
+    // string b="wallah";
+    // cout<<a<<"\n"<<b;
+
+    // int a , b;
+    // cout<<"enter first number";
+    // cin>>a;
+    // cout<<"enter second number";
+    // cin>>b;
+    // int c=a+b;
+    // cout<<c;
+
+    int radius=8;
+    float circumference=2*3.14*radius;
+}
