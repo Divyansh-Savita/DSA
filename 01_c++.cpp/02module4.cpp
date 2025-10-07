1fabc7beac70b5d089ff1752e88cffb63768cfc2
Divyansh Savita <savitadivyansh323@gmail.com>
2025-10-07 17:44:01 +0530

    more questions sloved

---
+++ 01_c++.cpp/02module4.cpp
@@ -0,0 +1,51 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    //even or odd
+    // int a=3;
+    // if(a%2==0){
+    //     cout<<"even";
+    // }else{
+    //     cout<<"odd";
+    // }
+
+    //absolute value
+    // int nums=-5;
+    // if(nums>=0){
+    //     cout<<nums;
+    // }else{
+    //     cout<<-nums;//-0 is 0
+    // }
+    // if(nums<0){
+    //     nums=-nums;
+    // }
+    // cout<<nums;
+    // return 0;
+
+    //profit loss
+    // int sp;
+    // int cp;
+    // cout<<"gimme your selling price: ";
+    // cin>>sp;
+    // cout<<"gimme your cost price: ";
+    // cin>>cp;
+    // int result=sp-cp;
+    // if(sp>cp){
+    //     cout<<"Profit of ";
+    //     cout<<result;
+    // }else if(sp==cp){
+    //     cout<<"No profit/Loss";
+    // }
+    // else{
+    //     cout<<"Loss of";
+    //     cout<<result;
+    // }
+
+    //3 digit or not
+    int n=123;
+    if(n>=100 && n<=999){
+        cout<<"Three digit";
+    }else{
+        "not three digit";
+    }
+}
