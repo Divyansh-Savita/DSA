c39e26aae8f40abd47ead267cc05a14db4fb66fe
Divyansh Savita <savitadivyansh323@gmail.com>
2025-10-07 19:57:20 +0530

    questions added

--- 01_c++.cpp/02module4.cpp
+++ 01_c++.cpp/02module4.cpp
@@ -42,10 +42,108 @@ int main(){
     // }
 
     //3 digit or not
-    int n=123;
-    if(n>=100 && n<=999){
-        cout<<"Three digit";
-    }else{
-        "not three digit";
-    }
+    // int n=123;
+    // if(n>=100 && n<=999){
+    //     cout<<"Three digit";
+    // }else{
+    //     "not three digit";
+    // }
+
+    //number divisible by 5 and 3
+    // int n=127;
+    // if(n%5==0 && n%3==0){
+    //     cout<<"divisible by both";
+    // }else{
+    //     cout<<"not divisible by both";
+    // }
+
+    //number divisible by 5 or 3
+    // int n=127;
+    // if(n%5==0 || n%3==0){
+    //     cout<<"divisible by either 5 or 3";
+    // }else{
+    //     cout<<"not divisible by both";
+    // }
+
+    //greatest of 3
+    // int a,b,c;
+    // cin>>a;
+    // cin>>b;
+    // cin>>c;
+    // if(a>b && a>c){
+    //     cout<<a<<" is greatest";
+    // }else if(b>a && b>c){
+    //     cout<<b<<" is largest";
+    // }else{
+    //     cout<<c<<" is largest";
+    // }
+
+    //alphabet or not
+    // char ch='0';
+    // int ascii=(int)ch;
+    // if((ascii>=65 && ascii<=90) || (ascii>=97 && ascii<=122)){//logical and has more priority
+    //     cout<<"yes it is an alphabet";
+    // }
+    // else{
+    //     cout<<"not alphabet";
+    // }
+
+    //divisible by 5 or 3 but not by 15
+    // int n=18;
+    // if(n%5==0 || n%3==0){
+    //     if(n%15!=0){
+    //         cout<<"divisible by 5 or 3 but not by 15";
+    //     }
+    // }else{
+    //     cout<<"not fit for condition given";
+    // }
+
+    //marks of student and print grade
+    // int marks=95;
+    // if(marks>=91 && marks<=100){cout<<"Excellent";}
+    // else if(marks>=81 && marks<=90){cout<<"Very Good";}
+    // else if(marks>=71 && marks<=80){cout<<"Good";}
+    // else if(marks>=61 && marks<=70){cout<<"Can do better";}
+    // else if(marks>=51 && marks<=60){cout<<"Average";}
+    // else if(marks>=41 && marks<=50){cout<<"Below Average";}
+    // else if(marks<=40){cout<<"Fail";}
+
+    //vowel or consonant
+    // char ch='0';
+    // int ascii=(int)ch;
+    // if((ascii>=65 && ascii<=90) || (ascii>=97 && ascii<=122)){//logical and has more priority
+    //     if(ch=='a' || ch=='e' || ch=='i' || ch=='o' || ch=='u'){
+    //         cout<<'vowel';
+    //     }
+    // }
+    // else{
+    //     cout<<"not alphabet";
+    // }
+
+    //sides of triangle
+    // int a,b,c;
+    // cin>>a;
+    // cin>>b;
+    // cin>>c;
+    // if(a+b>c && b+c>a && a+c>b){
+    //     cout<<"it's a triangle";
+    // }
+
+    //ternary,switch
+    // int x=99;
+    // (x>90)?cout<<"mast":cout<<"ok-ok";
+
+    //pass or fail without using if else
+    // int marks=98;
+    // (marks>33)?cout<<"pass":cout<<"fail";
+
+    //ternary
+    // int x;
+    // x=5>8?10:1!=2<5?20:30;
+    // cout<<x;
+
+    //ternary
+    // int test=0;
+    // float f=3.111;
+    // cout<<(test>0?f:0)<<endl;
 }
