a2e485acef9c2cc20a5c283dfc5bdcf1e11b9c5f
Divyansh Savita <savitadivyansh323@gmail.com>
2025-12-16 17:40:31 +0530

    solve

--- 01_python/Solve.java
+++ 01_python/Solve.java
@@ -1,6 +1,8 @@
  void main(){
-        int i=2,j=3,k,l;
-        double a,b;
-        k=i/j*j;
-        System.out.println(k);
+        
+        var a="Ajay";
+        var age=23;
+        var percentage=74.4;
+        var branch="cse";
+        System.out.println(a+" of "+age+" year's old got "+percentage+"% in "+branch+" branch");
     }
